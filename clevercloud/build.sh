6a24a0d911638d62e609fbdb163c347946ca0e24
Sébastien Prunier <sebastien.prunier@serli.com>
2018-05-02 11:15:07 +0200

    Update build.sh

--- clevercloud/build.sh
+++ clevercloud/build.sh
@@ -2,6 +2,6 @@
 
 echo "Getting izanami jar"
 
-wget --quiet 'https://dl.bintray.com/maif/binaries/izanami.jar/latest/izanami.jar' -P ${APP_HOME}
+wget --quiet 'https://dl.bintray.com/maif/binaries/izanami.jar/1.0.4/izanami.jar' -P ${APP_HOME}
 
 
