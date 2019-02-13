dd87af5a800947be83c21f553e2992984bbb3de6
Sébastien Prunier <sebastien.prunier@serli.com>
2019-02-13 11:04:19 +0100

    Fix https://github.com/So-Use/infra/issues/82

--- clevercloud/build.sh
+++ clevercloud/build.sh
@@ -2,6 +2,6 @@
 
 echo "Getting izanami jar"
 
-wget --quiet 'https://dl.bintray.com/maif/binaries/izanami.jar/1.3.1/izanami.jar' -P ${APP_HOME}
+wget --quiet 'https://dl.bintray.com/maif/binaries/izanami.jar/1.4.0/izanami.jar' -P ${APP_HOME}
 
 
